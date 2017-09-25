2931cbc3843191a0643939bb10082dc3fb7e96cc
HardPlant <abc7988se@naver.com>
2017-09-25 22:14:41 +0900

    Session Implemented :)

---
+++ sessionApp.py
@@ -0,0 +1,32 @@
+from flask import Flask, session, redirect, url_for, escape, request
+'''
+session implement cookie with encryption
+'''
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['useranme'])
+    return 'Not loggend in'
+
+@app.route('/login', method = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+    <form action "" method='post>
+        <p><input type=text name=username>
+        <p><input type=submit value=Login>
+    </form
+    '''
+@app,route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+app.secret_key = 'Something_Key_Do_Not_Hardcode'
+
+if __name__ == '__main__':
+    app.run()
