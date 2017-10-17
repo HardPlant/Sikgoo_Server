d1f1bf43cac125a6b7ae3dfa5d4c28b81cb58d2b
HardPlant <abc7988se@naver.com>
2017-10-17 09:35:35 +0900

    update

--- restaurant/restaurant/polls/views.py
+++ restaurant/restaurant/polls/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question, Choice
+import json
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    q = Question.objects.get(pk=1)
+    dicts = dict({
+        'question_text': q.question_text,
+        'pub_date' : str(q.pub_date)
+    })
+    return HttpResponse(json.dumps(dicts))
